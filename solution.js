bbaaf724609dd8ab5dd1f4f8b5bce8cc88665b6f
Chris Veenboer <chris.veenboer@adevinta.com>
2024-07-02 11:14:56 +0200

    Solution 459

--- solution.js
+++ solution.js
@@ -22,3 +22,4 @@ solution[228] = [ [false,0,9],  [false,1,5],  [true,1,4],   [false,0,16], [false
 solution[239] = [ [false,3,11], [true,0,20],  [false,2,1],  [false,2,8],  [false,0,21], [false,1,7]  ];
 solution[332] = [ [false,3,13], [false,5,21], [true,1,4],   [false,3,19], [false,2,23], [true,3,5]   ];
 solution[452] = [ [false,0,20], [false,0,15], [false,4,19], [false,2,7],  [false,1,18], [true,4,6]   ];
+solution[459] = [ [false,0,2],  [true,5,17],  [false,4,26], [false,3,19], [false,2,5],  [false,3,22] ];
