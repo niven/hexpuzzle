379e4888de5600624e5bffcb4fe6bec3a69b2dba
Chris Veenboer <chris.veenboer@adevinta.com>
2024-07-23 11:40:41 +0200

    Solution 303

--- solution.js
+++ solution.js
@@ -64,6 +64,7 @@ solution[262] = [ [false,0,12], [true,1,0],   [false,4,19], [false,2,11], [false
 solution[274] = [ [false,0,5],  [false,4,22], [false,2,1],  [false,5,20], [false,4,13], [false,1,12] ];
 solution[286] = [ [false,0,9],  [true,2,5],   [false,0,20], [false,1,1],  [false,4,13], [true,2,6]   ];
 solution[297] = [ [false,0,22], [true,1,0],   [false,4,19], [false,4,17], [false,5,9],  [false,4,25] ];
+solution[303] = [ [false,0,11], [true,4,7],   [false,5,22], [true,4,3],   [false,5,9],  [true,2,16]  ];
 solution[309] = [ [false,0,11], [false,5,20], [false,2,1],  [false,0,13], [false,0,21], [false,0,6]  ];
 solution[320] = [ [false,0,13], [false,4,21], [true,1,11],  [true,0,25],  [false,4,22], [false,5,10] ];
 solution[332] = [ [false,3,13], [false,5,21], [true,1,4],   [false,3,19], [false,2,23], [true,3,5]   ];
